6f6aa2fd543189ad46295d9366e4fb18b15f10a7
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-07-25 20:06:33 +0200

    started on iteration 14

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -3,6 +3,16 @@ import profiles from '../data/berlin.json';
 const Facebook = () => {
   return (
     <div className="facebook">
+        <div className='my-10'>
+            <button>England</button>
+            <button>USA</button>
+            <button>Malaysia</button>
+            <button>Germany</button>
+            <button>Sweden</button>
+            <button>Nigeria</button>
+            <button>Italy</button>
+        </div>
+
       {profiles.map((profile) => {
         return (
           <div className="profile flex text-left border border-black p-1 w-[500px] mx-auto mb-2">
